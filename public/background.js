b33b38a5a2e07032ef375cde65513f0fb347d7af
Julia Rosenrauch <jul_rose@yahoo.ca>
2022-01-16 09:17:47 -0800

    icon set and deep dive sorta working on static sites

--- public/background.js
+++ public/background.js
@@ -16,19 +16,21 @@ chrome.runtime.onMessage.addListener(async function(message, sender, senderRespo
 
 
 function addImage() {
-  let logo = chrome.runtime.getURL("happy.png");
+  let logo = chrome.runtime.getURL("happy2.png");
   var elemDiv = document.createElement('img');
   elemDiv.setAttribute("id", "blobby67");
-  elemDiv.style.cssText = 'position: fixed;right: 10px;bottom: 10px;z-index: 1000;width:125px;';
+  elemDiv.style.cssText = 'position: fixed;right: 10px;bottom: 10px;z-index: 1000;width:75px;';
   elemDiv.src = logo;
   document.body.appendChild(elemDiv);
-  var paragraphs = document.getElementsByClassName("css-axufdj")[0].textContent;
-    var enc_text = encodeURIComponent(paragraphs);
-    fetch(`https://api.us-south.tone-analyzer.watson.cloud.ibm.com/instances/9a31545b-cdc0-4cbb-8eb7-4bd1469db1c3/v3/tone?version=2017-09-21&text=${enc_text}`, {
-      headers: {
-        Authorization: "Basic YXBpa2V5OmdSM3ZLdE9zX1RuanFNd0p0Mk5VcEdBS0o2dm5FcE9GYzd2Q0gwOEhvTEox"
-      }
-    }).then(response => response.json()).then(response => {
-      console.log(response)
-    })
+  var paragraph = document.getElementsByClassName("css-axufdj")[0].textContent;
+  var enc_text = encodeURIComponent(paragraph);
+  fetch(`https://api.us-south.tone-analyzer.watson.cloud.ibm.com/instances/9a31545b-cdc0-4cbb-8eb7-4bd1469db1c3/v3/tone?version=2017-09-21&text=${enc_text}`, {
+    headers: {
+      Authorization: "Basic YXBpa2V5OmdSM3ZLdE9zX1RuanFNd0p0Mk5VcEdBS0o2dm5FcE9GYzd2Q0gwOEhvTEox"
+    }
+  }).then(response => response.json()).then(response => {
+    console.log(response)
+  })
+  //var sentiment = JSON.parse(response => response.json())
+
 }
