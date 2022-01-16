1b1bcfe58e79d1ff17b4a63922c2e2c826aab764
Nandini Bulusu <nanbul785@gmail.com>
2022-01-16 11:42:30 -0800

    last changes

--- public/background.js
+++ public/background.js
@@ -17,11 +17,15 @@ chrome.runtime.onMessage.addListener(async function(message, sender, senderRespo
 
 function addImage() {
   if (document.getElementById("blobby67") == null){
-    let logo = chrome.runtime.getURL("happy.png");
+    let logo = chrome.runtime.getURL("happy2.png");
     var elemDiv = document.createElement('img');
     elemDiv.setAttribute("id", "blobby67");
-    elemDiv.style.cssText = 'position: fixed;right: 10px;bottom: 10px;z-index: 1000;width:125px;';
+    elemDiv.style.cssText = 'position: fixed;right: 10px;bottom: 10px;z-index: 1000;width:75px;';
     elemDiv.src = logo;
+    elemDiv.onclick = function(){
+      elemDiv.src = chrome.runtime.getURL("angrypopup.png");
+      elemDiv.style.cssText = 'position: fixed;right: 10px;bottom: 10px;z-index: 1000;width:250px;';
+    };
     document.body.appendChild(elemDiv);
   }
 
@@ -63,23 +67,42 @@ function addImage() {
     // // })
     // })
 
-    fetch(`https://api.us-south.tone-analyzer.watson.cloud.ibm.com/instances/e6e23a80-d1dd-4c73-98c4-d12c4623e5e3/v3/tone?version=2017-09-21&text=${enc_text}`, {
+    fetch(`https://api.us-south.tone-analyzer.watson.cloud.ibm.com/instances/d1ccf867-64e5-4e41-8387-befd0da6dfb6/v3/tone?version=2017-09-21&text=${enc_text}`, {
       headers: {
-        Authorization: "Basic YXBpa2V5OmF6US1rNnRreTZCQ3lEWE12SVBpcVhKbnJDRXgwcFJMS0s0VDg0aW9kMFhi"
+        Authorization: "Basic YXBpa2V5OmJtS3QwLW5iUW1zR3RQdFVZY3gtRWh2a09nQmV5NDRrN3RxaTBweFVVV2xT"
       }
     }).then(response => response.json()).then(response => {
       emotion = response.document_tone.tones[0].tone_name;
+      // var atag = document.createElement('a');
+      // atag.href = "https://www.humanetech.com/take-control";
+      // atag.setAttribute("id", "blobbytag");
       var blob = document.getElementById("blobby67")
+      // atag.appendChild(blob)
       // curr_blob.style.display='none';
 
       if (emotion == "Fear") {
         blob.src = chrome.runtime.getURL("scared.png");
+        blob.onclick = function(){
+          blob.src = chrome.runtime.getURL("fearpopup.png");
+          blob.style.cssText = 'position: fixed;right: 10px;bottom: 10px;z-index: 1000;width:250px;';
+          blob.onclick = "opener.location='https://www.humanetech.com/take-control'"
+        };
       } else if (emotion == "Anger") {
         blob.src = chrome.runtime.getURL("angry.png");
+        blob.onclick = function(){
+          blob.src = chrome.runtime.getURL("angrypopup.png");
+          blob.style.cssText = 'position: fixed;right: 10px;bottom: 10px;z-index: 1000;width:250px;';
+          blob.onclick = "opener.location='https://www.humanetech.com/take-control'"
+        };
       } else if (emotion == "Sadness") {
         blob.src = chrome.runtime.getURL("scared.png");
+        blob.onclick = function(){
+          blob.src = chrome.runtime.getURL("fearpopup.png");
+          blob.style.cssText = 'position: fixed;right: 10px;bottom: 10px;z-index: 1000;width:250px;';
+          blob.onclick = "opener.location='https://www.humanetech.com/take-control'"
+        };
       } else {
-        blob.src = chrome.runtime.getURL("happy.png");
+        blob.src = chrome.runtime.getURL("happy2.png");
       }
       console.log(emotion)
     //   response.document_tone.tones.forEach((x) => {
