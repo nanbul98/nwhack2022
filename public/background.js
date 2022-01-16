b711c3c47ca39fc41dd34a801fb61601ce09653c
Julia Rosenrauch <jul_rose@yahoo.ca>
2022-01-16 02:40:15 -0800

    change to snorkel when deep, upload other emojis

--- public/background.js
+++ public/background.js
@@ -14,14 +14,12 @@ chrome.runtime.onMessage.addListener(async function(message, sender, senderRespo
   }
 });
 
+
 function addImage() {
-      // var require_script = document.createElement('script');
-      // require_script.setAttribute("src","assets/js/require.js");
-      // document.head.appendChild(require_script);
-      let logo = chrome.runtime.getURL("happy.png");
+      let logo = chrome.runtime.getURL("happy2.png");
       var elemDiv = document.createElement('img');
       elemDiv.setAttribute("id", "blobby67");
-      elemDiv.style.cssText = 'position: fixed;right: 10px;bottom: 10px;z-index: 1000;width:125px;';
+      elemDiv.style.cssText = 'position: fixed;right: 10px;bottom: 10px;z-index: 1000;width:75px;';
       elemDiv.src = logo;
       document.body.appendChild(elemDiv);
       fetch("https://api.us-south.tone-analyzer.watson.cloud.ibm.com/instances/e9db5587-e4e6-479b-b4ee-7af1cdc6454e/v3/tone?version=2017-09-21&text=Team%2C%20I%20know%20that%20times%20are%20tough%21%20Product%20sales%20have%20been%20disappointing%20for%20the%20past%20three%20quarters.%20We%20have%20a%20competitive%20product%2C%20but%20we%20need%20to%20do%20a%20better%20job%20of%20selling%20it%21", {
---
+++ public/content.js
@@ -0,0 +1,38 @@
+var depthBottomMeters = 8; //Depth in meters
+var depthBottomPixel;
+var depthStart;
+
+var init = function(){
+
+	depthBottomPixel = meterToPixel(depthBottomMeters);
+	depthStart = depthBottomPixel - meterToPixel(depthBottomMeters * 0.4);
+
+	$(window).scroll(function(e){
+
+		var s = $("body, html").scrollTop();
+		var docHeight = document.body.scrollHeight;
+
+		var progress = (s - depthStart) / (depthBottomPixel - depthStart);
+		if(progress > -0.2){
+			// Prevent further scrolling
+			$("body, html").scrollTop(depthBottomPixel);
+			elemDiv = document.getElementById("blobby67")
+			elemDiv.src = chrome.runtime.getURL("snorkler.png")
+			console.log("in too deep")
+		} 
+	});
+
+};
+
+function meterToPixel(m){
+	// Using 96DPI
+	var p = ((m * 100) / 2.54) * 96;
+	return p;
+}
+
+
+window.addEventListener("load", async () => {
+    chrome.runtime.sendMessage({msg: 'image'});
+});
+
+window.addEventListener("load", async () => {init()});
