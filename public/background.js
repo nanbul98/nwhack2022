f67fd5eb5cab2d8b70d47b9eca4451834c68e6de
Nandini Bulusu <nanbul785@gmail.com>
2022-01-16 09:13:00 -0800

    changed to ap

--- public/background.js
+++ public/background.js
@@ -25,7 +25,9 @@ function addImage() {
   elemDiv.style.cssText = 'position: fixed;right: 10px;bottom: 10px;z-index: 1000;width:125px;';
   elemDiv.src = logo;
   document.body.appendChild(elemDiv);
-  var paragraphs = document.getElementsByClassName("css-axufdj")[0].textContent;
+  // var paragraphs = document.getElementsByClassName("css-axufdj")[0].textContent;
+  var ap = document.getElementsByClassName("Component-root-0-2-55")[0].textContent;
+  var emotion = "";
   // var anger = 0;
   // var fear = 0;
   // var joy = 0;
@@ -39,24 +41,45 @@ function addImage() {
   // var modeEmotion;
   // var text = "";
   // for(var i = 0; i < paragraphs.length; i++) {
-    var enc_text = encodeURIComponent(paragraphs);
+    // var enc_text = encodeURIComponent(paragraphs);
+    var enc_ap = encodeURIComponent(ap);
+    console.log(enc_ap)
     // console.log(enc_text)
     // fetch(`https://api.us-south.tone-analyzer.watson.cloud.ibm.com/instances/e9db5587-e4e6-479b-b4ee-7af1cdc6454e/v3/tone?version=2017-09-21&text=${enc_text}`, {
     //   headers: {
     //     Authorization: "Basic YXBpa2V5OmpTY3ZuMVVKWkdKMXJIMVgwRm9VckNzU3Q2QXV4WlNHdEdvTDlSRnZZNzFW"
     //   }
     // })
-    fetch(`https://api.us-south.tone-analyzer.watson.cloud.ibm.com/instances/9a31545b-cdc0-4cbb-8eb7-4bd1469db1c3/v3/tone?version=2017-09-21&text=${enc_text}`, {
+    // fetch(`https://api.us-south.tone-analyzer.watson.cloud.ibm.com/instances/9a31545b-cdc0-4cbb-8eb7-4bd1469db1c3/v3/tone?version=2017-09-21&text=${enc_text}`, {
+    //   headers: {
+    //     Authorization: "Basic YXBpa2V5OmdSM3ZLdE9zX1RuanFNd0p0Mk5VcEdBS0o2dm5FcE9GYzd2Q0gwOEhvTEox"
+    //   }
+    // }).then(response => response.json()).then(response => {
+    //   emotion = response.document_tone.tones[0].tone_name;
+    //   console.log(emotion)
+    // //   response.document_tone.tones.forEach((x) => {
+    // //     console.log(x, "tone")
+    // //     emotionCount[emotions.indexOf(x.tone_id)] += x.score
+    // // })
+    // })
+
+    fetch(`https://api.us-south.tone-analyzer.watson.cloud.ibm.com/instances/9a31545b-cdc0-4cbb-8eb7-4bd1469db1c3/v3/tone?version=2017-09-21&text=${enc_ap}`, {
       headers: {
         Authorization: "Basic YXBpa2V5OmdSM3ZLdE9zX1RuanFNd0p0Mk5VcEdBS0o2dm5FcE9GYzd2Q0gwOEhvTEox"
       }
     }).then(response => response.json()).then(response => {
-      console.log(response)
+      emotion = response.document_tone.tones[0].tone_name;
+      // if (emotion == "Fear") {
+      //   chrome.runtime.getURL("scared.png");
+      // }
+      console.log(emotion)
     //   response.document_tone.tones.forEach((x) => {
     //     console.log(x, "tone")
     //     emotionCount[emotions.indexOf(x.tone_id)] += x.score
     // })
     })
+
+
   // }
   // maxEmotionCount = Math.max(...emotionCount)
   // modeEmotion = emotions[emotionCount.indexOf(maxEmotionCount)]
